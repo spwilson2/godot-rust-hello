4e643e4dc10d1b32b2ce86cc866ad844c2d6e40b
Sean Wilson <spwilson27@gmail.com>
2023-06-07 00:55:27 -0700

    Add test of spawning ~10k entities from rust (it's slow)
    Somehow with my shitty TCP implementation, Unity is able to significantly
    outperform Godot in this. Unity is able to spawn 10k in ~40ms while godot takes
    100x that long...

--- godot-frontend/src/gdexample.cpp
+++ godot-frontend/src/gdexample.cpp
@@ -15,18 +15,67 @@ GDExample::GDExample() {
 GDExample::~GDExample() {
 }
 
+static GDExample *second = NULL;
+
+static std::mutex mtx;
+static std::vector<Vector2> instantiate_queue;
+static uint32_t count;
+
 extern "C" { 
     void hello_world();
+    void done_world();
+
+    void gds_instantiate(uint32_t x, uint32_t y) {
+        auto vec = Vector2(x,y);
+        //mtx.lock();
+        instantiate_queue.push_back(vec);
+        //mtx.unlock();
+        //auto new_node = (GDExample*) second->duplicate();
+        //auto trans = Transform2D(0, vec);
+        //new_node->set_transform(trans);
+        //return (uintptr_t)new_node;
+    }
+    void gds_move(uintptr_t id, uint32_t x, uint32_t y) {
+        auto node = (GDExample*)id;
+        auto vec = Vector2(x,y);
+        auto trans = Transform2D(0, vec);
+        node->set_transform(trans);
+    }
 }
 
+
 void GDExample::_process(double delta) {
-    if (!enabled) {
-        hello_world();
+    if (enabled) {
         return;
     }
-    time_passed += delta;
 
-    Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 20.0)), 10.0 + (10.0 * cos(time_passed * 10.5)));
+    if (!second ) {
+        second = (GDExample*) this->duplicate();
+        auto vec = Vector2(20,200);
+        auto trans = Transform2D(0, vec);
+        second->set_transform(trans);
+        this->add_sibling(second);
+        // Spawn the rust thread.
+        hello_world();
+    }
 
-    set_position(new_position);
+    //mtx.lock();
+    if (instantiate_queue.size() == 10000) {
+    while (instantiate_queue.size() > 0) {
+        Vector2 vec = instantiate_queue.back();
+        instantiate_queue.pop_back();
+        auto new_node = (GDExample*) this->duplicate();
+        auto trans = Transform2D(0, vec);
+        new_node->set_transform(trans);
+        this->add_sibling(new_node);
+        count++;
+        new_node->set_z_index(count% 4096);
+    }
+    done_world();
+}
+    //mtx.unlock();
+
+    //time_passed += delta;
+    //Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 20.0)), 10.0 + (10.0 * cos(time_passed * 10.5)));
+    //set_position(new_position);
 }
--- rust-server/src/lib.rs
+++ rust-server/src/lib.rs
@@ -1,5 +1,44 @@
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        OnceLock,
+    },
+    thread,
+    time::{Duration, SystemTime},
+};
+
+extern "C" {
+    pub fn gds_move(id: usize, x: u32, y: u32);
+    pub fn gds_instantiate(x: u32, y: u32);
+}
+
+static mut CELL: OnceLock<AtomicBool> = OnceLock::new();
+
+fn run_loop() {
+    unsafe {
+        let time = SystemTime::now();
+        for i in 0..10000 {
+            let x = fastrand::f32() * 500.0;
+            let y = fastrand::f32() * 500.0;
+            gds_instantiate(x as u32, y as u32);
+        }
+        while !CELL.get().unwrap().load(Ordering::Acquire) {
+            thread::sleep(Duration::from_micros(1));
+        }
+        println!("{:?}", time.elapsed().unwrap().as_millis());
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn done_world() {
+    CELL.get_mut().unwrap().store(true, Ordering::Release);
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn hello_world() {
     println!("Hello world");
+    let handle = std::thread::spawn(run_loop);
+    std::mem::forget(handle);
+
+    CELL.set(AtomicBool::new(false)).unwrap();
 }
